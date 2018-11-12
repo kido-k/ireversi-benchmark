2670b71b516549b5577cdce9384bc95aa837513a
DESKTOP-Q7A8B81\kidok <kotaro.k13@gmail.com>
2018-11-13 01:47:16 +0900

    mnodify logic

--- src/test/kido.js
+++ src/test/kido.js
@@ -28,19 +28,21 @@ function initPosition() {
   for (let i = 0; i <= SIZE; i += 1) {
     pieces[i] = new Array(SIZE).fill(0);
   }
-  pieces[HOSEI][HOSEI] = 1;
+  pieces[conv(0)][conv(0)] = 1;
 }
+
+const conv = (n) => {
+  return n + HOSEI;
+} 
 
 function getPieces() {
   const re_pieces = [];
-  for(let i = 0; i <= SIZE; i += 1){
-    for(let j = 0; j <= SIZE; j += 1){
+  for (let i = 0; i <= SIZE; i += 1) {
+    for (let j = 0; j <= SIZE; j += 1) {
       const userId = pieces[i][j];
-      if(pieces[i][j] !==0 && pieces[i][j] !==undefined){
-        let x = j -HOSEI;
-        let y = i -HOSEI;
-        const obj = {x,y,userId}
-        re_pieces.push(obj);
+      if (pieces[i][j] !== 0 && pieces[i][j] !== undefined) {
+        let x = j - HOSEI, y = i - HOSEI;
+        re_pieces.push({ x, y, userId });
       }
     }
   }
@@ -48,38 +50,35 @@ function getPieces() {
 }
 
 function judgePiece(x, y, userId) {
-  if (pieces[y + HOSEI][x + HOSEI] !== 0) {
+  if (pieces[conv(y)][conv(x)] !== 0) {
     return false;
   }
 
   let doneReverse = false;
 
-  if (user[userId] > 0) {
-    // １枚目か、２枚目以降かをチェック
+  if (user[userId] > 0) {   // １枚目か、２枚目以降かをチェック
     for (let dir of dirAll) {
       const reversePieces = makeReversePieces(
         y + dir[1], x + dir[0], userId, dir, []
       );
-      if (reversePieces) {
-        // ひっくり返す駒があればuserIdを書き換える
+      if (reversePieces) {      // ひっくり返す駒があればuserIdを書き換える
         doneReverse = true;
-        for (let xy of reversePieces) {
-          user[pieces[xy[1] + HOSEI][xy[0] + HOSEI]] -= 1;
+        for (let rp of reversePieces) {
+          user[rp[2]] -= 1;
           user[userId] += 1;
-          pieces[xy[1] + HOSEI][xy[0] + HOSEI] = userId;
+          pieces[conv(rp[1])][conv(rp[0])] = userId;
         }
       }
     }
     if (doneReverse) {
-      pieces[y + HOSEI][x + HOSEI] = userId;
+      pieces[conv(y)][conv(x)] = userId;
       user[userId] += 1;
       return true;
     }
   } else {
-    for (let dir of dirXY) {
-      //四方のどこかに他者の駒があるかどうかをチェック
-      if (pieces[y + dir[1] + HOSEI][x + dir[0] + HOSEI] !== 0) {
-        pieces[y + HOSEI][x + HOSEI] = userId;
+    for (let dir of dirXY) {      //四方のどこかに他者の駒があるかどうかをチェック
+      if (pieces[conv(y + dir[1])][conv(x + dir[0])] !== 0) {
+        pieces[conv(y)][conv(x)] = userId;
         user[userId] += 1;
         return true;
       }
@@ -89,10 +88,11 @@ function judgePiece(x, y, userId) {
 }
 
 function makeReversePieces(y, x, userId, dir, array) {
-  if (pieces[y + HOSEI][x + HOSEI] === 0) {
+  const piece = pieces[conv(y)][conv(x)];
+  if (piece === 0) {
     return false;
-  } else if (pieces[y + HOSEI][x + HOSEI] !== userId) {
-    array.push([x, y]);
+  } else if (piece !== userId) {
+    array.push([x, y, piece]);
     return makeReversePieces(y + dir[1], x + dir[0], userId, dir, array);
   } else {
     return array;
